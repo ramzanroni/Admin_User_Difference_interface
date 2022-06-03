629beb1251182c38a86f23d8c8d49fdcab38ef34
Ramzan <mdramzanroni76@gmail.com>
2022-06-04 01:14:13 +0600

    Laravel Blog

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::check()) 
+        {
+           if(Auth::user()->role_as=='1')
+           {
+            return $next($request);
+           }
+           else
+           {
+               return redirect('/home')->with('status', 'Access Denied ! As you are not an Admin');
+           }
+        }
+        else
+        {
+            return redirect('/login')->with('status', 'Please Login first');
+        }
+        
+    }
+}
